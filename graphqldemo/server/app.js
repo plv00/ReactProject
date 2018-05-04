e2c3fe8fb72aa0d87159682a2bf62ecfbfe3b6c5
Peter <plv829@gmail.com>
2018-05-03 22:32:51 -0400

    mutations

---
+++ graphqldemo/server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// connect to mlab database
+
+mongoose.connect('mongodb://test:test@ds119306.mlab.com:19306/gql-demo')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
